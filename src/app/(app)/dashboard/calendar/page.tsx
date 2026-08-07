4054b366eef7a8e99315475d4e4e07a95e23db36
Logan Scheid <scheid.logan@gmail.com>
2026-08-06 22:00:45 -0400

    added initial calendar view

--- src/app/(app)/dashboard/calendar/page.tsx
+++ src/app/(app)/dashboard/calendar/page.tsx
@@ -1,5 +1,34 @@
-﻿import { CalendarView } from "@/features/calendar/components/calendar-view";
+﻿'use client';
+
+import { CalendarView } from '@/features/calendar/components/calendar-view';
+import dayGridPlugin from '@fullcalendar/react/daygrid';
+import timeGridPlugin from '@fullcalendar/react/timegrid';
+import listPlugin from '@fullcalendar/react/list';
+import multiMonthPlugin from '@fullcalendar/react/multimonth';
 
 export default function CalendarPage() {
-  return <CalendarView />;
+    return (
+        <CalendarView
+            plugins={[
+                dayGridPlugin,
+                timeGridPlugin,
+                listPlugin,
+                multiMonthPlugin,
+            ]}
+            addButton={{
+                text: 'Add Event',
+                click() {
+                    alert('handle add event...');
+                },
+            }}
+            availableViews={[
+                'dayGridMonth',
+                'timeGridWeek',
+                'timeGridDay',
+                'listWeek',
+                'multiMonthYear',
+            ]}
+            initialView="dayGridMonth"
+        />
+    );
 }
